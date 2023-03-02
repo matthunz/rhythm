3b3d57b2cfa550ff268e3d05825d609e864ee1ce
Matt Hunzinger <matthunz2@gmail.com>
2023-03-02 12:07:39 -0500

    Refactor and make Task last_run an option

--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,51 @@
-use embedded_time::{
-    duration::{Duration, Microseconds, Milliseconds},
-    fixed_point::FixedPoint,
-    rate::{Hertz, Rate},
-    Clock, Instant,
-};
+use embedded_time::{duration::Milliseconds, fixed_point::FixedPoint, Clock, Instant};
 
 pub struct Task<C: Clock> {
     f: fn(),
     priority: u8,
     period: Milliseconds<C::T>,
-    last_run: Instant<C>,
+    last_run: Option<Instant<C>>,
 }
 
 impl<C: Clock> Task<C> {
-    pub fn ready(&self, now: Instant<C>) -> Option<C::T> {
-        let elapsed: Milliseconds<C::T> = now
-            .checked_duration_since(&self.last_run)
-            .unwrap()
-            .try_into()
-            .unwrap();
-
-        if elapsed >= self.period {
-            let ms = (elapsed - self.period) / self.period.integer();
-            Some(ms.integer())
-        } else {
-            None
+    pub fn new(period: Milliseconds<C::T>, f: fn()) -> Self {
+        Self {
+            f,
+            priority: 0,
+            period,
+            last_run: None,
         }
+    }
+
+    pub fn with_priority(mut self, priority: u8) -> Self {
+        self.priority = priority;
+        self
+    }
+
+    pub fn ready(&self, now: Instant<C>) -> Option<C::T> {
+        if let Some(last_run) = self.last_run {
+            let elapsed: Milliseconds<C::T> = now
+                .checked_duration_since(&last_run)
+                .unwrap()
+                .try_into()
+                .unwrap();
+
+            if elapsed >= self.period {
+                let ms = (elapsed - self.period) / self.period.integer();
+                return Some(ms.integer());
+            }
+        }
+
+        None
+    }
+
+    pub fn reset(&mut self) {
+        self.last_run = None;
     }
 
     pub fn run(&mut self, now: Instant<C>) {
         (self.f)();
-        self.last_run = now;
+        self.last_run = Some(now);
     }
 }
 
