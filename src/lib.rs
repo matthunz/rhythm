ae8a4decabcf7b8c551101f9c41a0963983606de
Matt Hunzinger <matthunz2@gmail.com>
2023-03-02 09:48:15 -0500

    Create Task and Scheduler structs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,51 @@
+use std::time::{Duration, SystemTime};
+
+pub struct Task {
+    f: fn(),
+    priority: u8,
+    freq: u32,
+    last_run: SystemTime,
+}
+
+impl Task {
+    pub fn is_ready(&self, now: SystemTime) -> bool {
+        let elapsed = now.duration_since(self.last_run).unwrap();
+        let freq_duration = Duration::from_secs_f32(1.0 / self.freq as f32);
+
+        elapsed >= freq_duration
+    }
+}
+
+
+pub struct Scheduler<const N: usize> {
+    tasks: [Task; N],
+}
+
+impl<const N: usize> Scheduler<N> {
+    pub fn new(tasks: [Task; N]) -> Self {
+        Self { tasks }
+    }
+
+    pub fn next_task(&mut self) ->  Option<&Task> {
+        let now = SystemTime::now();
+        let mut next: Option<&Task> = None;
+
+        for task in &self.tasks {
+            if task.is_ready(now) {
+                if let Some(next_task) = next {
+                    if task.priority > next_task.priority {
+                        next = Some(next_task)
+                    }
+                } else {
+                    next = Some(task);
+                }
+            }
+        }
+
+        next
+    }
+}
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
